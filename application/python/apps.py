ec585666af21fd40f19d210e4f0d8d844a9f7dd5
syuya-tanaka <84017791+syuya-tanaka@users.noreply.github.com>
2022-10-21 17:56:23 +0900

    テキストから抜き取る箇所をinput()で入力可能に変更とexecutorの名前変更
    input()で入力可能にした理由は、
    抜き取る箇所を選ぶ事ができる様にするため。
    executorの名前変更は文字数オーバーだったため。

--- application/python/apps.py
+++ application/python/apps.py
@@ -97,6 +97,43 @@ class PdfOperator(Explorer):
                 'exist': os.path.exists(OUTPUT_FILE)
             })
 
+    @staticmethod
+    def input_value_decision(value: str, decision_list: list) -> bool | None:
+        """Determine the input value and pass processing to each."""
+        lower_value = value.lower()
+        if lower_value in decision_list:
+            return True
+        return None
+
+    @staticmethod
+    def extract_place_determining() -> None:
+        """This is where you decide where to finally pull out."""
+        global START_OF_LINE
+        global END_OF_LINE
+
+        is_list_yes = ['y', 'yes']
+        is_list_no = ['n', 'no']
+
+        # is_defaultに"y"もしくは"Y"、"yes"、"Yes"等が入力された場合、デフォルト値を使用する。
+        is_default = input('output.txt内のどこからどこまでを抽出したいか入力してください\n'
+                           'デフォルトを使用しますか? y/n: ')
+
+        # デフォルトを使用する場合の処理。
+        if PdfOperator.input_value_decision(is_default, is_list_yes):
+            print(f'開始行: {START_OF_LINE}\n終了行: {END_OF_LINE}')
+
+        # デフォルト値を使用せず、値を受け付ける場合の処理。
+        if PdfOperator.input_value_decision(is_default, is_list_no):
+            start_line = input('初めの行数を入力してください: ')
+            end_line = input('終わりの行数を入力してください: ')
+
+            try:
+                START_OF_LINE = int(start_line)
+                END_OF_LINE = int(end_line)
+            except ValueError as err:
+                print('数値のみを受け付けます。もう一度入力をしてください。\n', err)
+                PdfOperator.extract_place_determining()
+
 
 class TranslateOperator(object):
     """pdfから抽出したデータを翻訳し、DBに注入する"""
@@ -131,8 +168,8 @@ class TranslateOperator(object):
     def trans_and_put_in_db_eng_to_jpn(self, queue) -> Any:
         """英語から日本語に翻訳してDBに入れる。"""
         print('Thread running...')
-        with concurrent.futures.ThreadPoolExecutor(max_workers=300) as executor:
-            futures = [executor.submit(
+        with concurrent.futures.ThreadPoolExecutor(max_workers=300) as exec:
+            futures = [exec.submit(
                         self.trans_eng_to_jpn,
                         word_count,
                         queue
@@ -153,6 +190,7 @@ def main() -> None:
     pdf_operator = PdfOperator()
     pdf_operator.fetch_word()
     raw_data = extract.Extractor(OUTPUT_FILE)
+    pdf_operator.extract_place_determining()
     formatted_data = raw_data.exec_extract_eng(START_OF_LINE, END_OF_LINE)
     trans_object = TranslateOperator(formatted_data, FROM_LANG, TO_LANG)
     trans_object.trans_and_put_in_db_eng_to_jpn(queue)
